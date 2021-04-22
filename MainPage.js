35648e4fad7abcd255f516bb43f9781ade16b293
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2021-04-22 17:16:22 +0900

    fixing

--- MainPage.js
+++ MainPage.js
@@ -2,12 +2,9 @@ var slideIndex = 0;
 
 var slides = document.getElementsByClassName("slideComponent");
 var dots = document.getElementsByClassName("dot");
-console.log(slides, slides[0]);
-for (let i = 0; i < slides.length; ++i) { console.log(i , " : ", slides[i]); }
 
 function showSlides() {
     for (let i = 0; i < slides.length; ++i) {
-        console.log(slides[i]);
         slides[i].style.display = "none";  
     }
     ++slideIndex;
@@ -15,7 +12,6 @@ function showSlides() {
     for (let i = 0; i < dots.length; ++i) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    console.log(slideIndex, slides[slideIndex-1]);
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 2000); // Change image every 2 seconds
